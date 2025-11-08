9a7db2dc62f8ad507446d6fed23390e792921c47
maleesha02 <weerasooriyamaleesha@gmail.com>
2025-11-08 23:47:33 +0530

    added new files

---
+++ todo-app-web/scripts/main.js
@@ -0,0 +1,18 @@
+function getCookie(name){
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+    }
+    return null;
+
+}
+document.getElementById('btnCreate')
+    .addEventListener('click', function() {
+        const token = getCookie('token');
+        if(token){
+            window.location.href = 'dashboard.html';
+        } else {
+            window.location.href = 'login.html';
+        }
+    });
