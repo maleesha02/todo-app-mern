ddd2a6f22a3c58834e8832f409886fe62b09afac
maleesha02 <weerasooriyamaleesha@gmail.com>
2025-11-16 11:26:32 +0530

    Files Updated

--- todo-app-web/scripts/main.js
+++ todo-app-web/scripts/main.js
@@ -1,18 +1,19 @@
-function getCookie(name){
+/*function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
         return parts.pop().split(';').shift();
     }
     return null;
+}*/
 
-}
 document.getElementById('btnCreate')
-    .addEventListener('click', function() {
-        const token = getCookie('token');
+    .addEventListener('click', function () {
+        const token = localStorage.getItem('token');
         if(token){
-            window.location.href = 'dashboard.html';
-        } else {
-            window.location.href = 'login.html';
+            window.location.href='dashboard.html';
+        }else{
+            window.location.href='login.html';
         }
     });
+
